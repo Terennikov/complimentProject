97e907d3460cc28e9cd2153973d7f22be119b71b
Daniil Terennikov <tupacs2002@gmail.com>
2023-10-23 01:22:06 +0300

    JsUpd+AdaptivFixes

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const popup = document.querySelector('.popup');
 const input = document.querySelector('.input');
 const submitButton = document.querySelector('.button');
 const submitForm = document.querySelector('.form');
+const compliment = document.querySelector('.compliment')
 
+const items = ['Улыбайся чаще, твоя улыбка осветила бы даже Питер']
 
 submitForm.addEventListener('submit', (event) => {
     event.preventDefault();
     popup.classList.add('popup_opened');
     submitForm.classList.add('form_closed')
+    textMassage()
   })
 
-// submitButton.addEventListener('click', () => {
-//   popupAdd.classList.add('popup_opened');
-// })
+function textMassage() {
+  const name = input.value;
+  compliment.textContent = name + ', ты не первый блин, но когда ты рядом в горле комом...';
+
+}
+
